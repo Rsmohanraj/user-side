270f3adf75f312dd3653dd17f037652ea6aa529e
Rsmohanraj <154896181+Rsmohanraj@users.noreply.github.com>
2024-09-21 21:43:29 +0530

    Update CartAction.js

--- src/Actions/CartAction.js
+++ src/Actions/CartAction.js
@@ -5,7 +5,7 @@ import { addCartItemRequest, addCartItemSuccess } from "../Slices/CartSlice"
 export const addToCart =(id, quantity) => async(dispatch) => {
     try {
         dispatch(addCartItemRequest())
-        const { data } = await axios.get(`https://server-side-0nct.onrender.com/api/v1/product/${id}`)
+        const { data } = await axios.get(`https://server-side-16.onrender.com/api/v1/product/${id}`)
         dispatch(addCartItemSuccess({
             product:data.product._id,
             name: data.product.name,
